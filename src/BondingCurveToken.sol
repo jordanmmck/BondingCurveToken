4723adbb9a5ad5d82f352facc41df59d600870ba
Jordan McKinney <jordanmmck@gmail.com>
2023-07-06 18:37:13 -0700

    init commit

--- src/BondingCurveToken.sol
+++ src/BondingCurveToken.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BondingCurveToken is ERC20 {
     uint256 public number;
+
+    constructor() ERC20("BondingCurveToken", "BCT") {
+        this;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
