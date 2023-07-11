beeac3d17a0b25abc24b05a37a21f047952cde04
Jordan McKinney <jordanmmck@gmail.com>
2023-07-10 17:54:21 -0700

    mint and burn working on curve

--- src/BondingCurveToken.sol
+++ src/BondingCurveToken.sol
@@ -2,19 +2,32 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 contract BondingCurveToken is ERC20 {
-    uint256 public number;
-
     constructor() ERC20("BondingCurveToken", "BCT") {
         this;
     }
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    function mintOnCurve() external payable {
+        uint256 startSupply = totalSupply();
+        uint256 endSupply = Math.sqrt(
+            2 * msg.value + startSupply * startSupply
+        );
+        uint256 mintAmount = endSupply - startSupply;
+
+        _mint(msg.sender, mintAmount);
     }
 
-    function increment() public {
-        number++;
+    function burnOnCurve(uint256 amount) public {
+        uint256 startSupply = totalSupply();
+
+        _burn(msg.sender, balanceOf(msg.sender));
+
+        uint256 endSupply = startSupply - amount;
+        uint256 ethOwing = (startSupply * startSupply - endSupply * endSupply) /
+            2;
+
+        payable(msg.sender).transfer(ethOwing);
     }
 }
--- test/BondingCurveToken.t.sol
+++ test/BondingCurveToken.t.sol
@@ -4,21 +4,72 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/BondingCurveToken.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract BondingCurveTokenTest is Test {
+    BondingCurveToken public bondingCurveToken;
+    address public jordan = address(0x34);
+    address public vitalik = address(0x77);
+    address public sam = address(0x99);
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        bondingCurveToken = new BondingCurveToken();
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function _mint(uint256 amount) private {
+        (bool ok, ) = address(bondingCurveToken).call{value: amount}(
+            abi.encodeWithSignature("mintOnCurve()")
+        );
+        require(ok, "send failed");
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testMint() public {
+        hoax(jordan, 1 ether);
+        _mint(1 ether);
+
+        hoax(vitalik, 1 ether);
+        _mint(1 ether);
+
+        hoax(sam, 1 ether);
+        _mint(1 ether);
+
+        assertGt(
+            bondingCurveToken.balanceOf(jordan),
+            bondingCurveToken.balanceOf(vitalik)
+        );
+        assertGt(
+            bondingCurveToken.balanceOf(vitalik),
+            bondingCurveToken.balanceOf(sam)
+        );
+    }
+
+    function testBurn() public {
+        // mint tokens
+        hoax(jordan, 1 ether);
+        _mint(1 ether);
+
+        // burn tokens
+        uint256 balance = bondingCurveToken.balanceOf(jordan);
+        vm.prank(jordan);
+        bondingCurveToken.burnOnCurve(balance);
+    }
+
+    function testProfitAndLoss() public {
+        hoax(jordan, 1 ether);
+        _mint(1 ether);
+        hoax(vitalik, 1 ether);
+        _mint(1 ether);
+
+        uint256 jBalance = bondingCurveToken.balanceOf(jordan);
+        vm.prank(jordan);
+        bondingCurveToken.burnOnCurve(jBalance);
+
+        // assert first buyer made profit
+        assertGt(jordan.balance, 1 ether);
+
+        uint256 vBalance = bondingCurveToken.balanceOf(vitalik);
+        vm.prank(vitalik);
+        bondingCurveToken.burnOnCurve(vBalance);
+
+        // assert second buyer had loss
+        assertLt(vitalik.balance, 1 ether);
     }
 }
